b8e531eb1e98cac5c94e04e4eba6ae39e7d07fcb
Gustavo Gimenez <gimenezanderson@gmail.com>
2024-06-26 01:05:03 -0400

    first commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Gustavo Gimenez on 26-06-24.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
